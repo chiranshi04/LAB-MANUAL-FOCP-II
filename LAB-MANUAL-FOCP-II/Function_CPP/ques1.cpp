adf909db54577c5856081783d4866686346f3c44
Chiranshi <chiranshi25csu056@ncuindia.edu>
2026-02-11 10:40:12 +0530

    Function lab manual updated

---
+++ LAB-MANUAL-FOCP-II/Function_CPP/ques1.cpp
@@ -0,0 +1,28 @@
+// function is prime or not checking
+
+#include<iostream>
+using namespace std;
+bool isprime(int n)
+{
+    if (n<=1)
+    return false;
+    for(int i=2;i<=n/2;i++)
+    {
+        if (n%i==0)
+        return false;
+    }
+    return true;
+
+}
+int main()
+{
+    int n;
+    cout<<"enter a number";
+    cin>>n;
+    
+    if (isprime(n))
+    cout<<"prime";
+    else
+    cout<<"not prime";
+    return 0;
+}
