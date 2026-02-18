32e3e86e9fcb5c5dc328dc98978fab72390f0faa
Chiranshi <chiranshi25csu056@ncuindia.edu>
2026-02-18 10:24:10 +0530

    commit 17th feb

---
+++ LAB-MANUAL-FOCP-II/ques32.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[5];
+    int count =0;
+    for(int i=0;i<5;i++)
+    {
+        cout<<"enter the number"<<i+1<<"="<<endl;
+        cin>>arr[i];
+    }
+ 
+
+}
